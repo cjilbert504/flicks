accfec7c7c4961950f2464b44b9f198239dc5440
Collin Jilbert <cjilbert504@gmail.com>
2020-11-01 15:56:23 -0600

    Show users errors if any during creating or editing a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,13 +17,20 @@ class MoviesController < ApplicationController
 
 	def update
 		@movie = Movie.find(params[:id])
-		@movie.update(movie_params)
-		redirect_to @movie
+		if @movie.update(movie_params)
+			redirect_to @movie
+		else
+			render :edit
+		end
 	end
 
 	def create
-		@movie = Movie.create(movie_params)
-		redirect_to @movie
+		@movie = Movie.new(movie_params)
+		if @movie.save
+			redirect_to @movie
+		else
+			render :new
+		end
 	end
 
 	def destroy
