b61f75eb6bd231539bbd6f7094ae7a7e8907cb95
Collin Jilbert <cjilbert504@gmail.com>
2020-11-02 16:21:33 -0600

    Add reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,6 +11,8 @@ class Movie < ApplicationRecord
 	RATINGS = %w(G PG PG-13 R NC-17)
 	validates :rating, inclusion: { in: RATINGS }
 
+	has_many :reviews, dependent: :destroy	
+
 	def flop?
 		total_gross.blank? || total_gross < 50000000
 	end
