a9eb2c8b390f124d0d8ac2a5c6ac8d179dbfc378
Collin Jilbert <cjilbert504@gmail.com>
2020-12-06 19:05:36 -0600

    Add spec and finish feature to have link to sign up page if user doesn't have an account

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe "Signing in a user" do
 		expect(page).to have_field("Email")
 		expect(page).to have_field("Password")
 	end
+
+	it "has a link to sign up if the user doesn't have an account" do
+		visit new_session_path
+
+		expect(page).to have_text("Don't have an account? Sign Up")
+	end
 end
