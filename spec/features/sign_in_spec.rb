1f5fe9f0aa7e836fe97f4926ae19325d171c6ed3
Collin Jilbert <cjilbert504@gmail.com>
2020-12-14 14:23:44 -0600

    Add current user helper and conditional logic to render proper links in header

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "Signing in a user" do
 		expect(page).to have_text("Welcome back, #{user.name}")
 		expect(page).to have_link(user.name)
 		expect(page).not_to have_link("Sign In")
-		expect(page).not_to have_liink("Sign Up")
+		expect(page).not_to have_link("Sign Up")
 	end
 
 	it "renders the sign in form again if authentication is unsuccessful" do
