c7e52cb5a1a22cdf1a9924c1e6c8b21c1cb09918
Collin Jilbert <cjilbert504@gmail.com>
2020-12-14 14:53:35 -0600

    Implement signing out a user when the user deletes their account

--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Deleting a user" do
 	it "destroys the user and redirects to the home page" do
 		user = User.create!(user_attributes)
-
+		
 		visit user_path(user)
 		click_link "Delete Account"
 
@@ -13,5 +13,16 @@ RSpec.describe "Deleting a user" do
     visit users_path
 
     expect(page).not_to have_text(user.name)
+	end
+
+	it "automatically signs out that user" do
+		user = User.create!(user_attributes)
+
+		sign_in(user)
+		visit user_path(user)
+		click_link "Delete Account"
+
+		expect(page).to have_link('Sign In')
+		expect(page).not_to have_link('Sign Out')
 	end
 end
