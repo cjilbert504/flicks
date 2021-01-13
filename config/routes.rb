e37c35fdd4ad3c6190e3589cd918d0cec46568ba
Collin Jilbert <cjilbert504@gmail.com>
2021-01-13 15:04:13 -0600

    finish user fave movies feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :favorites
   root "movies#index"
-
+  
   get "/signup" => "users#new"
   resources :users
-
+  
   get "/signin" => "sessions#new"
   resource :session
-
+  
   resources :movies do
     resources :reviews
+    resources :favorites
   end
 end
