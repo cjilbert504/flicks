ce4d0df4200e00743cabe4508b3ea5a0e3e85187
Collin Jilbert <cjilbert504@gmail.com>
2021-01-13 16:05:29 -0600

    add specs for movie genres; create genre resource; migrate; add seeds for genres

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -5,6 +5,9 @@ describe "Creating a new movie" do
   before do
     admin = User.create!(user_attributes(admin: true))
     sign_in(admin)
+    @genre1 = Genre.create!(name: "Genre 1")
+    @genre2 = Genre.create!(name: "Genre 2")
+    @genre3 = Genre.create!(name: "Genre 3")
   end
   
   it "saves the movie and shows the new movie's details" do
@@ -27,12 +30,17 @@ describe "Creating a new movie" do
     # If you're taking advantage of the HTML 5 date field in Chrome,
     # you'll need to use 'fill_in' rather than 'select'
     # fill_in "Released on", with: (Time.now.year - 1).to_s
+    check(@genre1.name)
+    check(@genre2.name)
 
     click_button 'Create Movie'
 
     expect(current_path).to eq(movie_path(Movie.last))
     expect(page).to have_text('Movie successfully created!')
     expect(page).to have_text('New Movie Title')
+    expect(page).to have_text(@genre1.name)
+    expect(page).to have_text(@genre2.name)
+    expect(page).not_to have_text(@genre3.name)
   end
 
   it "does not save the movie if it's invalid" do
